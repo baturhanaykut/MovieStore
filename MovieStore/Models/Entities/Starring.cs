5e42b3db27db8cf1600a1a1248496faf5e6be084
Baturhan Aykut <baturhanaykut@gmail.com>
2023-03-14 21:51:22 +0300

    Entities were updated.

---
+++ MovieStore/Models/Entities/Starring.cs
@@ -0,0 +1,21 @@
+﻿namespace MovieStore.Models.Entities
+{
+    public class Starring
+    {
+        public Starring()
+        {
+            PerformedMovies = new List<Movie>();
+        }
+
+        public int? Id { get; set; }
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        //Navigation Property
+        public List<Movie>? PerformedMovies { get; set; }
+
+    }
+}
