5d1ce53e8237cbe9f39ac56322745d53bf2aeaff
Baturhan Aykut <baturhanaykut@gmail.com>
2023-03-14 21:53:34 +0300

    Mapping classes of the entites were created. Context class created and adjusments in the programcs and appsettings were done for database connection

---
+++ MovieStore/Models/DataAccess/Mappigs/LanguageMapping.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovieStore.Models.Entities;
+
+namespace MovieStore.Models.DataAccess.Mappigs
+{
+    public class LanguageMapping : IEntityTypeConfiguration<Language>
+    {
+        public void Configure(EntityTypeBuilder<Language> builder)
+        {
+            builder.ToTable("Languages");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .UseIdentityColumn(1, 1)
+                .HasColumnOrder(1);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(30)
+                .IsUnicode(true)
+                .HasColumnOrder(2);
+        }
+    }
+}
---
+++ MovieStore/Models/DataAccess/Mappigs/StarringMapping.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovieStore.Models.Entities;
+
+namespace MovieStore.Models.DataAccess.Mappigs
+{
+    public class StarringMapping : IEntityTypeConfiguration<Starring>
+    {
+       
+        public void Configure(EntityTypeBuilder<Starring> builder)
+        {
+            builder.ToTable("Starrings");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .UseIdentityColumn(1, 1)
+                .HasColumnOrder(1);
+
+            builder.Property(x => x.FirstName)
+                .IsRequired()
+                .HasMaxLength(30)
+                .IsUnicode(true)
+                .HasColumnOrder(2);
+
+
+            builder.Property(x => x.LastName)
+                .IsRequired()
+                .HasMaxLength(30)
+                .IsUnicode(true)
+                .HasColumnOrder(3);
+
+            builder.Property(x => x.BirthDate)
+               .IsRequired()
+               .HasColumnType("date")
+               .HasColumnOrder(4);
+        }
+    }
+}
