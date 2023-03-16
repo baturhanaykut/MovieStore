2c082e12db7dbebc3f82591f020cca5f11fa20d7
Baturhan Aykut <baturhanaykut@gmail.com>
2023-03-17 00:27:28 +0300

    Display names were added to the Director View Model.

--- MovieStore/Models/ViewModels/DirectorVM.cs
+++ MovieStore/Models/ViewModels/DirectorVM.cs
@@ -8,13 +8,16 @@ namespace MovieStore.Models.ViewModels
         public int? Id { get; set; }
         [Required(ErrorMessage ="First Name cannnot be null")]
         [MaxLength(30, ErrorMessage="The maxiumum lengt of first name can be 30 characters")]
+        [Display(Name ="First Name")]
         public string? FirstName { get; set; }
-        
+
+        [Display(Name = "Last Name")]
         [Required(ErrorMessage = "Last Name cannnot be null")]
         [MaxLength(30, ErrorMessage = "The maxiumum lengt of last name can be 30 characters")]
         public string? LastName { get; set; }
         [Required(ErrorMessage = "Birth Date cannnot be null")]
         [DataType(DataType.Date)]
+        [Display(Name = "Birth Date")]
         public DateTime? BirthDate { get; set; }
 
         public List<Movie>? DirectedMovies { get; set; }
